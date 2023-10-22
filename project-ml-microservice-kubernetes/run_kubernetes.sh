26cd977acfbabe7092acfc86211c5b049830c9f6
EC2 Default User <ec2-user@ip-172-31-77-181.ec2.internal>
2023-10-22 18:15:32 +0000

    updating project

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="awslambda2023/devopsmicroservice:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run devopsmicroservice \
+    --image=awslambda2023/devopsmicroservice:v1.0.0 \
+    --port=80 --labels app=devopsmicroservice
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward devopsmicroservice 8080:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="awslambda2023/devopsmicroservice:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag devopsmicroservice $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
